468b80e55bb3cbdfdef0dd7fa80db21510d1ee43
valeroy31 <midstorm31@gmail.com>
2025-03-16 18:11:52 +0100

    use-state-practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import React,{useState} from "react";
-
-// useState is used to define a state you want to change on the webpage. i.e. A live html
-// update
-// useState can only be used when first imported, as seen above
+import React, { useState } from 'react'
 
 const App = () => {
-  // const [nameOfVariable, functionToChangeVariable] = useState(initialValue)
 
-  const [myNum, setMyNum] = useState(10) // This is a state variable. The use state hook is used
-  // to define a state variable. The first value is the variable name, the second value is the
-  // function to change the variable and the third value is the initial value of the variable
-  // The function to change the variable is used to change the value of the variable. It is only used
-  // when there has to be an active change to a variable value on the webpage
+const [myAge, setmyAge] = useState(18)
 
- 
-   const increment = (inc) => {
-    setMyNum( myNum+inc)
-    
-    console.log(myNum)
-   }
-  return (
-    <div>
-      <h1>Hello, {myNum}</h1>
-      <button onClick={()=> increment(10)}>Increment</button>
-      {/* // if you want to pass a parameter to a function in the onClick event, you have to use an arrow
-      // function to call the function. The arrow function is used to call the function with the
-      // parameter. The parameter is then passed to the function */}
-    </div>
-  );
+const increaseAge=(inc)=>{
+
+  setmyAge(myAge+inc)
 }
 
-export default App;
+
+
+  return (
+    <div>
+      my age is {myAge}
+      <button onClick={(inc)=>increaseAge(10)} >Increase age</button>
+    </div>
+  )
+}
+
+export default App
